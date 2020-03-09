07b17f61561814831bccf89c62a7ed4473e7d8d5
SHUBHAM <prakashshubham36@gmail.com>
2020-03-09 16:36:32 +0530

    jenkins file

---
+++ JenkinsTest/src/com/jenkins/Greet.java
@@ -0,0 +1,16 @@
+package com.jenkins;
+
+public class Greet {
+
+	static void printMe(String name)
+	{
+		System.out.println("Welcom "+name);
+	}
+	public static void main(String[] args) {
+		String name="Shubham";
+		Greet.printMe(name);
+		
+
+	}
+
+}
