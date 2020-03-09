f7cec7acd372ad4ffbcfbb2514708f02132a32c1
SHUBHAM <prakashshubham36@gmail.com>
2020-03-09 17:24:55 +0530

    maven jenkins

---
+++ myJavaApp/src/main/java/ibm_JavaFullstack/myJavaApp/App.java
@@ -0,0 +1,13 @@
+package ibm_JavaFullstack.myJavaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Shubham!" );
+    }
+}
