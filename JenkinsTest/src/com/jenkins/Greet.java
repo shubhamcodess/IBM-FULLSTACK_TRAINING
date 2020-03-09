cc1e295ac3226b7752bc1844ae578186eb1b6a5e
SHUBHAM <prakashshubham36@gmail.com>
2020-03-09 16:38:41 +0530

    jenkins

--- JenkinsTest/src/com/jenkins/Greet.java
+++ JenkinsTest/src/com/jenkins/Greet.java
@@ -4,7 +4,7 @@ public class Greet {
 
 	static void printMe(String name)
 	{
-		System.out.println("Welcom "+name);
+		System.out.println("Welcome "+name);
 	}
 	public static void main(String[] args) {
 		String name="Shubham";
